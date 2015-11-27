18c651b6e2b525cf7de5ce34a11fb23fb49f2350
Clauder <clauderlima@gmail.com>
2015-11-27 17:34:06 -0200

    Adicionado os Labels e faltando editar restrição

--- module/Census/src/Census/Controller/RestricaoController.php
+++ module/Census/src/Census/Controller/RestricaoController.php
@@ -10,70 +10,6 @@ class RestricaoController extends AbstractController
 	{
 		$dataRestricao = $this->getEm('Census\Entity\Restricaomedica')->findAll();
 		
-/* 		$query = $this->getEm()->createQueryBuilder()
-			->select('r.rescodigo,r.inicio,r.fim')
-			->from('\Census\Entity\Restricaomedica','r')
-			->getQuery(); */
-		
-/* 		$query = $this->getEm()->createQueryBuilder()
-			->select('p.codigo,p.postograduacao,p.nomeguerra,r.rescodigo,r.inicio,r.fim')
-			->from('\Census\Entity\Restricaomedica','r')
-			->innerJoin('r.polcodigo','p')
-			->where('r.polcodigo = p.codigo')
-			->getQuery(); */
-		
-		
-		//@TODO Colocar essa lógica no Model e verificar porque o doctrine não está trazendo as informaçõse
-		
-/* 		$query = $this->getEm()->createQueryBuilder()
-			->select('p.codigo,p.postograduacao,p.nomeguerra,r.rescodigo,r.inicio,r.fim,t.tipo')
-			->from('\Census\Entity\Restricaomedica','r')
-			->innerJoin('r.polcodigo','p')
-			->where('r.polcodigo = p.codigo')
-			->innerJoin('r.retcodigo','t')
-			->orderBy('p.antiguidade', 'ASC')
-			->getQuery();
-		 
-		$dataRestricao = $query->getResult(); */
-		
-		/* $dataPolicial = array();
-		
-		foreach ($dataRestricao as $item)
-		{
-			foreach ($item as $chave => $valor)
-			{
-				switch ($chave) {
-					case 'codigo':
-						$policial['codigo'] = $valor;
-					break;
-					case 'postograduacao':
-						$policial['postograduacao'] = $valor;
-					break;
-					case 'nomeguerra':
-						$policial['nomeguerra'] = $valor;
-					break;
-					case 'rescodigo':
-						$policial['rescodigo'] = $valor;
-					break;
-					case 'inicio':
-						$policial['inicio'] = $valor->format('d/m/Y');
-					break;
-					case 'fim':
-						$policial['fim'] = $valor->format('d/m/Y');
-					break;
-			
-				}
-			echo "<br>zz" . $item['codigo'] . "  " . $item['tipo'];
-			}
-			$dataPolicial[] = $policial;
-		}
-		
-		echo "<br>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<br>";
-		echo "<pre>";
-		print_r($dataPolicial);
-		
-		echo "<br>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<br>";
-		 */
 		return new ViewModel(array(
 			'dados' => $dataRestricao,
 		));
@@ -174,6 +110,56 @@ class RestricaoController extends AbstractController
 	
 		// Instaciando o Form
 		$form = new \Census\Form\Restricaomedica($this->getServiceLocator()->get('servicemanager'));
+	
+		if ($request->isPost())
+		{
+			// setando o input filter no orm
+			$data = $request->getPost()->toArray();
+			$form->setData($data);
+			$form->setInputFilter(new \Census\Filter\Restricaomedica());
+	
+			if ($form->isValid())
+			{
+				if ($serviceRestricaomedica->insert($data, 'Census\Entity\Restricaomedica'))
+				{
+					$this->flashMessenger()->addSuccessMessage("CTGRAFI cadastrado com sucesso!");
+					return $this->redirect()->toUrl('/restricao');
+				}
+			} else {
+				$this->flashMessenger()->addErrorMessage('Erro ao cadastrar arma! <br>Verifique se os campos foram preenchidos corretamente.');
+			}
+		}
+			
+		$view = new ViewModel(array(
+				'form' => $form,
+				'policial' => $dataPolicial
+		));
+			
+		$view->setTemplate('census/restricao/form-rm.phtml');
+			
+		return $view;
+	}
+	
+	public function alterarAction()
+	{
+		// Definindo variaveis
+		$request = $this->getRequest();
+		$id = (int) $this->params()->fromRoute('id', 0);
+	
+		// Instaciando services
+		$serviceRestricaomedica = $this->getServiceLocator()->get('census-service-restricaomedica');
+		$dataPolicial = $this->getEm('Census\Entity\Policial')->find($id)->toArray();
+	
+		// Instaciando o Form
+		$form = new \Census\Form\Restricaomedica($this->getServiceLocator()->get('servicemanager'));
+		$restricaomedica = $this->getEm('Census\Entity\Restricaomedica')->find($id)->toArray();
+		
+		echo "<pre>";
+		print_r($restricaomedica);
+		exit;
+		
+		
+		$form->setData($restricaomedica);
 	
 		if ($request->isPost())
 		{
